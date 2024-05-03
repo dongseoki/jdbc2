88932e458f58a2ea69398c1c93a6a1d2621227c0
dongseoki <43495319+dongseoki@users.noreply.github.com>
2024-05-03 21:06:41 +0900

    10 스프링 트랜잭션 전파1 기본 - 외부롤백 -> 외부 트랜잭션에서 물리 트랜잭션의 롤백이 일어난다.

--- springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -100,4 +100,21 @@ public class BasicTxTest {
 		txManager.commit(outer);
 	}
 
+	@Test
+	void outer_rollback() {
+		log.info("외부 트랜잭션 시작");
+		TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+		//DB INSERT A
+
+		log.info("내부 트랜잭션 시작");
+		TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+		log.info("내부 트랜잭션 커밋");
+
+		//DB INSERT B
+		txManager.commit(inner);
+
+		log.info("외부 트랜잭션 롤백");
+		txManager.rollback(outer);
+	}
+
 }
