2740ac938860fe344a3f49b8e243be67e2b4271c
dongseoki <43495319+dongseoki@users.noreply.github.com>
2024-05-03 21:14:15 +0900

    10 스프링 트랜잭션 전파1 기본 - requires_new 하면 서로 안건드림.

--- springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ springtx/src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -2,6 +2,7 @@ package hello.springtx.propagation;
 
 import javax.sql.DataSource;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +10,9 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 
 import lombok.extern.slf4j.Slf4j;
@@ -121,7 +124,6 @@ public class BasicTxTest {
 	void inner_rollback() {
 		log.info("외부 트랜잭션 시작");
 		TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
-		//DB INSERT A
 
 		log.info("내부 트랜잭션 시작");
 		TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
@@ -130,7 +132,29 @@ public class BasicTxTest {
 		txManager.rollback(inner); // rollback-only 표시
 
 		log.info("외부 트랜잭션 커밋");
+		Assertions.assertThatThrownBy(() -> txManager.commit(outer))
+			.isInstanceOf(UnexpectedRollbackException.class);
+
+	}
+
+	@Test
+	void inner_rollback_requires_new() {
+		log.info("외부 트랜잭션 시작");
+		TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+		log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+		log.info("내부 트랜잭션 시작");
+		DefaultTransactionAttribute definition = new DefaultTransactionAttribute();
+		definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+		TransactionStatus inner = txManager.getTransaction(definition);
+		log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+
+		log.info("내부 트랜잭션 롤백");
+		txManager.rollback(inner);
+
+		log.info("외부 트랜잭션 커밋");
 		txManager.commit(outer);
+
 	}
 
 }
