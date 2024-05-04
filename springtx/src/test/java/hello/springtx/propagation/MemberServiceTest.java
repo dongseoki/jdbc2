21b7f8ca4631c185c23b17424ee1659520d1a4e5
Dong Seok Lee <dongseok.lee.log@gmail.com>
2024-05-04 15:03:33 +0900

    10 트랜잭션 전파 활용- 커밋 롤백. logRepo만 롤백이 잘됨을 확인.

--- springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ springtx/src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -33,7 +34,20 @@ class MemberServiceTest {
 
 	}
 
+	/**
+	 * MemberService @Transactional:OFF
+	 * MemberRepository @Transactional:ON
+	 * LogRepository @Transactional:ON Exception
+	 */
 	@Test
-	void joinV2() {
+	void outerTxOff_fail() {
+		//given
+		String username = "로그예외_outerTxOff_fail";
+		//when
+		assertThatThrownBy(() -> memberService.joinV1(username))
+			.isInstanceOf(RuntimeException.class);
+		//then: 완전히 롤백되지 않고, member 데이터가 남아서 저장된다.
+		assertTrue(memberRepository.find(username).isPresent());
+		assertTrue(logRepository.find(username).isEmpty());
 	}
 }
